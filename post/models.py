f381fcbfb9a58b10c018778c6b5798274646f24e
ahmedazab1235 <ahmedazab1235@yahoo.com>
2020-12-31 13:48:26 +0200

    this the first trial for post and blog

---
+++ post/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+CATEGORY_CHOICES = (
+    ('DataBase deal','will view'),
+    ('',''),
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField()
+
+    published = models.BooleanField(default=True)
+    email = models.EmailField(max_length=254)
+    views_count = models.IntegerField(default=0,blank=True, null=True)
+    CATEGORY = models.CharField(choices = CATEGORY_CHOICES, max_length=20)
+
+    def __str__(self):
+        return self.title
+    
